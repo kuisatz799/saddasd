7c0355dbfd2d7471a8677978b962e8c2c07ddc04
kuisatz799 <143664471+kuisatz799@users.noreply.github.com>
2026-04-20 14:18:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let example = 20
+
+let x;
+
+let my_cat;
+
+x = 30
+
+console. log(example)
+
+console.log(x)
+
+console.log(my_cat)
+
+my_cat = 50
+
+console.log(my_cat)
+
+const coding = 80
+
+console.log(coding)
